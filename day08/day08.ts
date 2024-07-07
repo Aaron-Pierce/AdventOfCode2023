9e1e5c2581714d0dec7786f789d4fbad1987a16c
Aaron <aaronmp01@gmail.com>
2024-07-06 22:54:46 -0700

    Day 8

--- day08/day08.ts
+++ day08/day08.ts
@@ -1,49 +1,178 @@
 import { readFileSync } from 'fs';
-import { Parser, character, combine, make_parser, skip, whitespace, word } from "../parser_combinator";
+import { character, exactly, make_parser, skip, word } from "../parser_combinator";
 import { ident } from '../utils';
+import { assert } from 'console';
 
-function part1(directions: string, lines: [[string, string], string][]) {
+
+function part1(directions: string, lines: [string, [string, string]][]) {
+    let networkMap = Object.fromEntries(lines);
     
+    let position = "AAA";
+    let steps = 0;
+
+    while(position !== "ZZZ") {
+        position = networkMap[position]["LR".indexOf(directions[steps++ % directions.length])];
+    }   
+
+    return steps;
 }
 
-function part2() {
 
+type LoopDescription = {
+    startPosition: string,
+    startStep: number
+}
+
+/*
+
+The general idea was:
+1. Ghosts have to loop, since the space of (position, index into direction string) is finite.
+2. We can generate an infinite list of the steps at which a particular ghost is on a terminal space from:
+(for some integer i which is >= 1)
+(steps until the loop start state) + i * (number of steps until reaching the terminal state) + (i-1) * (number of steps until getting back to the start of the loop).
+
+And then I didn't have a better idea about how to solve this than brute force:
+for every ghost we have a list of these generating functions, and we just iterate through all of the possible inputs to see if every ghost has some step in common.
+
+But the problem simplifies this for us:
+1. Every ghost's loop contains only one terminal state
+2. Every ghost's loop offset (number of steps before reaching the loop start state for the first time)
+is exactly equal to the number of steps it takes to go from the terminal state to the start of the loop again.
+For example: a loop which starts at step 3, and loops every 100 steps, will hit the terminal state after 97 steps through the loop.
+This simplifies the math a lot:
+(loopOffset) + i * loopStartToTerminal + (i-1)*terminalToLoopBeginning
+ => loopOffset + i * loopStartToTerminal + (i-1)*loopOffset
+ => i * (loopStartToTerminal + loopOffset)
+ => i * loopLength
+
+So every ghost will be on a terminal state every `loopLength` steps.
+So when will all ghosts be on a terminal state? The LCM of the loop lengths.
+Didn't bother to implement an LCM function, just use WolframAlpha!
+*/
+
+function part2(directions: string, inputLines: [string, [string, string]][]) {
+    let networkMap = Object.fromEntries(inputLines);
+    let positions = Object.keys(networkMap).filter(e => e.endsWith("A"));
+
+    // The one liner I used in part1 was gross
+    const advance_ = (pos, step) => {
+        let direction = directions[step % directions.length];
+        let directionAsIndex = "LR".indexOf(direction);
+        let nextPosition = networkMap[pos][directionAsIndex];
+        let nextStep = step + 1;
+        return [nextPosition, nextStep];
+    }
+
+    // For a ghost's start position, find the first repeated (state, step % directions.length) pair.
+    function findLoop(startPosition: string): LoopDescription {
+        let position = startPosition;
+        let step = 0;
+        let visited = new Set();
+        const memoKey = (_position: string, _step: number) => _position + "|" + (_step % directions.length);
+
+        while (!visited.has(memoKey(position, step))) {
+            visited.add(memoKey(position, step));
+            [position, step] = advance_(position, step);
+        }
+        
+
+        return {
+            startPosition: position,
+            startStep: step % directions.length
+        }
+    }
+
+    function findLoopOffsetAndLength(loop: LoopDescription, startPosition: string) {
+
+        // Walk until we hit the loop start state
+        let position = startPosition;
+        let step = 0;
+        while (!(position == loop.startPosition && (step % directions.length) == (loop.startStep % directions.length))) {
+            [position, step] = advance_(position, step);
+
+        }
+        
+        // That many steps is the offset.
+        // This offset is always equal to startPosition.startStep,
+        // so this is redundant.
+        const offset = step;
+        [position, step] = advance_(position, step);
+
+        // Walk the loop until we get back to the start state
+        while (!(position == loop.startPosition && (step % directions.length) == (loop.startStep % directions.length))) {
+            [position, step] = advance_(position, step);
+
+        }
+        
+        // The length of the loop is the number of steps that took.
+        const length = step - offset;
+        return [offset, length];
+
+    }
+
+    // Walk the loop and identify any time we cross a state which ends in a Z.
+    // We could just do this while computing the loop length, but that's cluttered
+    function findPotentialEndNodesAlongPath(startPosition: string, startStep: number, pathLength: number): [string, number][] {
+        let position = startPosition;
+        let step = startStep;
+        let finds:  [string, number][] = [];
+        for(let i = 0; i < pathLength; i++) {
+            
+            if(position.endsWith("Z")) finds.push([position, i]);
+            [position, step] = advance_(position, step);
+
+        }
+        return finds;
+    }
+
+    let generatingFunctions: ((i: number) => number)[] = [];
+    for(let ghostStart of positions) {
+
+        let loop = findLoop(ghostStart);
+        let [loopOffset, loopLength] = findLoopOffsetAndLength(loop, ghostStart);
+        let potentialEndNodes = findPotentialEndNodesAlongPath(loop.startPosition, loop.startStep, loopLength);
+        assert(potentialEndNodes.length === 1) // happens to be true for my input
+        let loopIndexOfEnd = potentialEndNodes[0][1];
+        assert((loopLength - loopIndexOfEnd) === loopOffset) // happens to be true for all of the loops
+        // Now, find the offset - how long it takes from t=0 to beginning the loop
+
+        let stepsWhenOnPotentialEndNode = (i) => loopOffset + (i * loopIndexOfEnd) + (i-1) * (loopLength - loopIndexOfEnd);
+        generatingFunctions.push(stepsWhenOnPotentialEndNode);
+    }
+    
+    // At this point, I realized that the generating functions were equivalent to i*loopLength.
+    // So this line is just equivalent to each loop's length
+    let poses = generatingFunctions.map(e => e(1));
+
+    console.log(`Go to WolframAlpha and plug in lcm(${poses.join(", ")})`)
 }
 
 
-// I wanted to write a tiny little type-safe parser combinator for this.
-// The dream was to pass in an array of parsers to a function,
-// and get a properly typed parser out.
-// So you could construct a parser like:
-// parser = make_parser([word, skip(equals_sign), word_tuple])
-// where word_tuple = make_parser(skip('('), word, skip(', '), word, skip(')'))
-// And then you could write parser("LRL = (MCG, TRC)")
-// And the type of the parser would be
-// Parser<[String, [String, String]]>.
-// and it would return ["LRL", ["MCG", "TRC"]]
-// 
-// And I've got that now, but only if you write out all of the combinations in source
+
 let [directions, networkInput] = readFileSync("input.txt").toString().split("\n\n");
 
 let word_tuple = make_parser(
     skip(character('(')),
     word(),
-    skip(character(',')),
-    skip(whitespace()),
+    skip(exactly(', ')),
     word(),
     skip(character(')'))
 );
 
 let input_line_parser = make_parser(
     word(),
-    skip(whitespace()),
-    skip(character('=')),
-    skip(whitespace()),
+    skip(exactly(" = ")),
     word_tuple
 )
 
 
 let parsed = networkInput.split("\n").filter(ident).map(input_line_parser).map(e => e.tokens);
-console.log(parsed[0]);
 
-// part1(directions, parsed);
+console.log(
+    part1(directions, parsed)
+)
+
+
+console.log(
+    part2(directions, parsed)
+)
--- parser_combinator.ts
+++ parser_combinator.ts
@@ -1,81 +1,79 @@
+// Given a string, and an object T, parses the serialized representation of T,
+// and returns the deserialized value and whatever's left of the input afterwards
+export type Parser<T> = (input: string) => ParsingOutput<T>;
 export interface ParsingOutput<T> {
     residue: string,
     tokens: T
 }
 
-export type Parser<T> = (input: string) => ParsingOutput<T>;
 
+// Given two parsers, P<A> and P<B>, which output an object A and B respectively,
+// what should P<A>(P<B>) output? 
+// Well, if both return a value, return both their outputs [OA, OB] (where OA is the output of parser A, similar. B)
+// If one is undefined, just return that one as a single value.
+// And if both are undefined, return undefined.
 type Bool = true | false;
 type NoToken<T> = T extends undefined ? true : false;
-type NotUndefined<T> = Not<NoToken<T>>; 
+type NotUndefined<T> = Not<NoToken<T>>;
 type Not<T extends Bool> = T extends true ? false : true;
 type And<T1 extends Bool, T2 extends Bool> = T1 extends true ? (T2 extends true ? true : false) : false;
-export type CombineTypes<A, B> = And<NotUndefined<A>, NotUndefined<B>> extends true 
-    ? [A, B] 
-    : (And<NoToken<A>, NoToken<B>> extends true 
+export type CombineTypes<A, B> = And<NotUndefined<A>, NotUndefined<B>> extends true
+    ? [A, B]
+    : (And<NoToken<A>, NoToken<B>> extends true
         ? undefined
         : NoToken<A> extends true
-            ? B
-            : A
-        );
+        ? B
+        : A
+    );
 
-export function combine<A, B>(p1: Parser<A>, p2: Parser<B>): Parser<CombineTypes<A, B>> {
+
+// Given two parsers, as described above, return a parser which outputs P<A>(P<B>)
+function combine<T1 extends Parser<undefined>, T2 extends Parser<undefined>>(p1: T1, p2: T2): Parser<undefined>;
+function combine<T1 extends Parser<unknown>, T2 extends Parser<undefined>>(p1: T1, p2: T2): Parser<Flatten<T1>>;
+function combine<T1 extends Parser<undefined>, T2 extends Parser<unknown>>(p1: T1, p2: T2): Parser<Flatten<T2>>;
+function combine<T1 extends Parser<unknown>, T2 extends Parser<unknown>>(p1: T1, p2: T2): Parser<[Flatten<T1>, Flatten<T2>]>;
+
+function combine<T1, T2>(p1: Parser<T1>, p2: Parser<T2>): Parser<CombineTypes<T1, T2>> {
     return (input: string) => {
         let out1 = p1(input);
         let out2 = p2(out1.residue);
 
-        let outputs = [out1.tokens, out2.tokens].filter(e => e !== undefined);
-        let toReturn = outputs.length === 0 ? undefined : outputs.length === 1 ? outputs[0] : outputs;
+        let tok1 = out1.tokens;
+        let tok2 = out2.tokens;
+        let filtered = [tok1, tok2].filter(e => e !== undefined);
+        
+        // TODO: try to figure out how to do this without a type assertion.
+        // May require a total re-think, typescript doesn't want to narrow
+        // T1 to undefined when p1(input).tokens is undefined.
+        // Need to understand this corner of ts better
         return {
             residue: out2.residue,
-            tokens: toReturn as CombineTypes<A, B>
-        }
+            tokens: (filtered.length === 0 ? undefined : filtered.length === 1 ? filtered[0] : filtered) as CombineTypes<T1, T2>
+        };
     }
 }
 
-// type Flatten<T> = T extends Parser<infer Inner> ? Inner : never;
-// type CombineParserList<T extends readonly [Parser<unknown>, ...Parser<unknown>[]]> = T[0] extends any ? T[""] : never;
-// type _CombineParserList<N extends number> = N;
-// type m = _CombineParserList<[1, 2, 3]>; 
-// type k = CombineParserList<[Parser<number>, Parser<undefined>, Parser<string>]>;
+// Given a list of parsers, produce the type from combining all of them from left to right.
 type Flatten<T extends Parser<unknown>> = T extends Parser<infer Inner> ? Inner : never;
 type ParserList = readonly [...Parser<unknown>[]];
-type Combined<AlreadyCombined extends ParserList, ToCombine extends ParserList, CombinedType extends unknown> = AlreadyCombined["length"] extends ToCombine["length"] ? CombinedType : Combined<[...AlreadyCombined, ToCombine[AlreadyCombined["length"]]], ToCombine, CombineTypes<Flatten<ToCombine[AlreadyCombined["length"]]>, CombinedType>>;
+type Combined<AlreadyCombined extends ParserList, ToCombine extends ParserList, CombinedType extends unknown> = AlreadyCombined["length"] extends ToCombine["length"] ? CombinedType : Combined<[...AlreadyCombined, ToCombine[AlreadyCombined["length"]]], ToCombine, CombineTypes<CombinedType, Flatten<ToCombine[AlreadyCombined["length"]]>>>;
 
-
-let pp = [
-    word(),
-    skip(whitespace()),
-    character(','),
-    whitespace()
-];
-
-function tupelize<T extends any[]>(...args: T): T {
-    return args;
-}
-
-let q = tupelize(
-    word(),
-    skip(whitespace()),
-    character(','),
-    whitespace()
-);
-
+// Given a list of parsers, produce a parser which is the composition of all of them from left to right
 export function make_parser<T extends Parser<unknown>[]>(...parsers: T): Parser<Combined<[], typeof parsers, undefined>> {
-    if(parsers.length === 1) throw "Can't combine only one parser - just use the parser!";
-    if(parsers.length === 2) return combine(parsers[0], parsers[1]) as any;
+    if (parsers.length === 1) throw "Can't combine only one parser - just use the parser!";
+    if (parsers.length === 2) return combine(parsers[0], parsers[1]) as any;
     const remainder = parsers.slice(1);
     return combine(parsers[0], make_parser(...remainder)) as any;
 }
-type fefesh = Combined<[], typeof q, undefined>;
-let res = make_parser(
-    word(),
-    skip(whitespace()),
-    character(','),
-    whitespace()
-)
 
 
+
+/*
+* The rest of the file is just a bunch of basic parsers which can be combined
+*/
+
+// Parse out the longest prefix of the string where each character
+// matches a particular regexp.
 export function contiguousMatch(regexp: RegExp): Parser<string> {
     return (input: string) => {
         let parsed = "";
@@ -91,6 +89,7 @@ export function contiguousMatch(regexp: RegExp): Parser<string> {
     }
 }
 
+// Parse out a single character
 export function character(char: string): Parser<string> {
     if (char.length !== 1) throw new Error(`A character parser may only be given one character, not '${char}'`)
     return (input: string) => {
@@ -101,13 +100,29 @@ export function character(char: string): Parser<string> {
         }
     }
 }
+
+// Parse out a string of english letters
 export function word(): Parser<string> {
     return contiguousMatch(/[A-Za-z]/);
 }
+
+// Parse out a contiguous block of whitespace
 export function whitespace(): Parser<string> {
     return contiguousMatch(/\s/);
 }
 
+// Parse out the given prefix of the string
+export function exactly(str: string): Parser<string> {
+    return (input: string) => {
+        if(input.substring(0, str.length) !== str) throw new Error(`Couldn't parse exact literal '${str}' from '${input}'`)
+        return {
+            residue: input.slice(str.length),
+            tokens: input.substring(0, str.length)
+        }
+    }
+}
+
+// Swallow the output of the given parser - don't add its tokens to the output.
 export function skip(parser: Parser<any>): Parser<undefined> {
     return (input) => {
         let output = parser(input);
